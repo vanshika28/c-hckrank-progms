01b95d2a393bd9b9bcc9e05b808c95d43f731eb1
vanshika <vanshikanonu1999@gmail.com>
2019-09-20 03:15:20 +0530

    C hackerank programs

---
+++ sumof2no.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main()
+
+    {int a,c,d,b ;
+float e, f, g,h;
+    scanf("%d %d",&a , &b);
+     c = a+b;
+     d = a-b;
+    printf("%d %d ", c,d );
+    printf("\n");
+    scanf(" %f %f",&e,&f);
+    
+    g=e+f;
+    h=e-f;
+    printf("%0.1f %0.1f" , g,h);
+
+    
+
+	
+    
+    return 0;
+}
+
